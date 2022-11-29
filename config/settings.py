48dd10e9236fd1530b5b454ee190c49301aee886
fedor <f.shatruk@thesis.team>
2022-11-29 13:19:45 +0300

    step_6

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "markdownify.apps.MarkdownifyConfig", # pip install django-markdownify
+
     "mainapp",
 ]
 
