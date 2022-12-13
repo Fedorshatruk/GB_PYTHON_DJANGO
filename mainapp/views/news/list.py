508096c76a3730d71bdf9ca53da77158f27fd745
fedor <f.shatruk@thesis.team>
2022-12-13 20:04:03 +0300

    step 1

--- mainapp/views/news/list.py
+++ mainapp/views/news/list.py
@@ -7,8 +7,16 @@ from mainapp.models import News
 
 class NewsListView(ListView):
     model = News
-    paginate_by = 5
+    paginate_by = 2
     # template_name = "mainapp/news/news_list.html"
 
     def get_queryset(self):
-        return super().get_queryset().filter(deleted=False)
+        qs = super().get_queryset().filter(deleted=False)
+        if self.request.GET.get('dateFrom'):
+            qs = qs.filter(created__gte=self.request.GET.get('dateFrom'))
+        if self.request.GET.get('dateTo'):
+            qs = qs.filter(created__lte=self.request.GET.get('dateTo'))
+        return qs
+    
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
